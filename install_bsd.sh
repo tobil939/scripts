6ee1b01868716f5e14ca2f254ec5b5171de4f7fb
Tobi L <tobias.leitz@gmx.de>
2025-08-06 21:34:52 +0200

    umstellung auf arch

--- install_bsd.sh
+++ install_bsd.sh
@@ -1,4 +1,4 @@
-#!/usr/local/bin/bash
+#!/bin/bash
 
 #=======================#
 #     PROGRAMMLISTEN    #
@@ -8,17 +8,17 @@ pkg_programme=(
   "sudo"
   "bash"
   "nano"
-  "openzfs"
+  "xfsprogs"
   "neovim"
   "kitty"
   "neofetch"
-  "python3"
+  "python"
   "npm"
-  "hs-ShellCheck"
+  "shellcheck"
   "shfmt"
-  "isc-dhcp44-server"
+  "dhcp"
   "clamav"
-  "samba420"
+  "samba"
   "git"
   "iftop"
   "nload"
@@ -26,6 +26,7 @@ pkg_programme=(
   "unbound"
   "smartmontools"
   "hostapd"
+  "iptables-nft"
 )
 
 npm_programme=(
@@ -50,7 +51,7 @@ log() {
 }
 
 fail() {
-  echo "[ERROR] $1"
+  echo "[ERROR] $1" >&2
   exit 1
 }
 
@@ -59,97 +60,44 @@ warn() {
 }
 
 install_pkg_programme() {
+  log "Installiere Pakete..."
+  pacman -Syu --noconfirm || fail "Systemupdate fehlgeschlagen"
   for pkg in "${pkg_programme[@]}"; do
     log "Installiere $pkg ..."
-    pkg install -y "$pkg" || fail "$pkg konnte nicht installiert werden"
+    pacman -S --noconfirm --needed "$pkg" || warn "$pkg konnte nicht installiert werden"
   done
 }
 
 install_npm_programme() {
   for npm_pkg in "${npm_programme[@]}"; do
     log "Installiere (npm) $npm_pkg ..."
-    npm install -g "$npm_pkg" --silent || fail "$npm_pkg konnte nicht installiert werden"
+    npm install -g "$npm_pkg" --silent || warn "$npm_pkg konnte nicht installiert werden"
   done
 }
 
 set_router_rechte() {
-  log "Richte router-Gruppe ein ..."
-  pw groupadd router 2>/dev/null
+  log "Erstelle router-Gruppe und füge Benutzer hinzu ..."
+  groupadd -f router
   for user in "${router_benutzer[@]}"; do
-    pw groupmod router -m "$user" || fail "Benutzer $user konnte nicht hinzugefügt werden"
+    usermod -aG router "$user" || warn "Benutzer $user konnte nicht hinzugefügt werden"
   done
 }
 
-setup_sudo() {
-  log "Füge sudo-Regel für router-Gruppe mit Passwort hinzu ..."
-  echo "%router ALL=(ALL) ALL" >>/usr/local/etc/sudoers || fail "Sudo-Konfiguration fehlgeschlagen"
-}
-
-setup_netwerk() {
-  log "Netzwerkschnittstellen konfigurieren ..."
-  sysrc ifconfig_em0="DHCP"
-  sysrc ifconfig_re0="inet 10.10.10.1 netmask 255.255.255.0"
-  ifconfig re0 inet 10.10.10.1 netmask 255.255.255.0
-  if ifconfig iwm0 >/dev/null 2>&1; then
-    sysrc ifconfig_iwm0="inet 10.10.10.2 netmask 255.255.255.0"
-    ifconfig iwm0 inet 10.10.10.2 netmask 255.255.255.0
+setup_netzwerk() {
+  log "Netzwerk konfigurieren ..."
+  ip addr add 10.10.10.1/24 dev enp7s0
+  ip link set enp7s0 up
+  if ip link show wlan0 >/dev/null 2>&1; then
+    ip addr add 10.10.10.2/24 dev wlan0
+    ip link set wlan0 up
   else
-    warn "iwm0 nicht gefunden – WLAN wird übersprungen"
+    warn "wlan0 nicht gefunden – WLAN wird übersprungen"
   fi
 }
 
-setup_dhcp() {
-  log "DHCP-Server einrichten ..."
-  sysrc dhcpd_enable="YES"
-  sysrc dhcpd_ifaces="re0 iwm0"
-}
-
-setup_ssh() {
-  log "SSH aktivieren ..."
-  sysrc sshd_enable="YES"
-  service sshd start || fail "sshd konnte nicht gestartet werden"
-}
-
-setup_wlan_ap() {
-  log "hostapd konfigurieren ..."
-  sysrc hostapd_enable="YES"
-}
-
-setup_samba() {
-  log "Samba aktivieren ..."
-  sysrc samba_server_enable="YES"
-}
-
-setup_firewall_pf() {
-  log "Firewall (pf) aktivieren ..."
-  sysrc pf_enable="YES"
-
-  cat <<EOF >/etc/pf.conf
-ext_if = "em0"
-lan_if = "re0"
-lan_net = "10.10.10.0/24"
-
-nat on \$ext_if from \$lan_net to any -> (\$ext_if)
-
-block all
-pass quick on lo0
-pass in quick on \$lan_if inet from \$lan_net to any keep state
-pass out quick on \$ext_if inet from any to any keep state
-EOF
-}
-
-setup_clamav() {
-  log "ClamAV aktivieren ..."
-  sysrc clamav_clamd_enable="YES"
-  sysrc clamav_freshclam_enable="YES"
-
-  log "Signaturen aktualisieren ..."
-  freshclam || fail "ClamAV Signaturen konnten nicht aktualisiert werden"
-}
-
 create_dhcpd_conf() {
-  log "Erstelle /usr/local/etc/dhcpd.conf ..."
-  cat <<EOF >/usr/local/etc/dhcpd.conf
+  log "Erstelle /etc/dhcp/dhcpd.conf ..."
+  cat <<EOF >/etc/dhcp/dhcpd.conf
 subnet 10.10.10.0 netmask 255.255.255.0 {
   range 10.10.10.100 10.10.10.200;
   option routers 10.10.10.1;
@@ -161,62 +109,60 @@ EOF
 }
 
 create_hostapd_conf() {
-  log "Erstelle /usr/local/etc/hostapd.conf ..."
-  cat <<EOF >/usr/local/etc/hostapd.conf
-interface=iwm0
+  log "Erstelle /etc/hostapd/hostapd.conf ..."
+  cat <<EOF >/etc/hostapd/hostapd.conf
+interface=wlan0
 ssid=$WlanName
 hw_mode=g
 channel=6
 auth_algs=1
 wpa=2
-wpa_passphrase=$WlanPW 
+wpa_passphrase=$WlanPW
 wpa_key_mgmt=WPA-PSK
 rsn_pairwise=CCMP
 EOF
 }
 
-create_smb4_conf() {
-  log "Erstelle /usr/local/etc/smb4.conf ..."
-  cat <<EOF >/usr/local/etc/smb4.conf
+create_smb_conf() {
+  log "Erstelle /etc/samba/smb.conf ..."
+  cat <<EOF >/etc/samba/smb.conf
 [global]
    workgroup = WORKGROUP
-   server string = FreeBSD Samba Server
+   server string = Arch Linux Samba Server
    security = user
-   passdb backend = tdbsam
-   load printers = no
-   log file = /var/log/samba4/log.%m
+   log file = /var/log/samba/%m.log
    max log size = 50
 
-[zfs1]
-   path = /mnt/zfs1
+[speicher11]
+   path = /mnt/speicher11
    read only = no
    browsable = yes
 
-[zfs2]
-   path = /mnt/zfs2
+[speicher12]
+   path = /mnt/speicher12
    read only = no
    browsable = yes
 EOF
 }
 
-stop_services() {
-  log "Stoppe alle Dienste ..."
-  service pf stop || warn "pf konnte nicht gestoppt werden"
-  service isc-dhcpd stop || warn "DHCP konnte nicht gestoppt werden"
-  service sshd stop || warn "sshd konnte nicht gestoppt werden"
-  service samba_server stop || warn "Samba konnte nicht gestoppt werden"
-  service hostapd stop || warn "hostapd konnte nicht gestoppt werden"
-  service clamav-clamd stop || warn "ClamAV konnte nicht gestoppt werden"
+setup_dienste() {
+  log "Aktiviere systemd-Dienste ..."
+  systemctl enable --now sshd
+  systemctl enable --now dhcpd4@enp7s0.service
+  systemctl enable --now hostapd
+  systemctl enable --now smb
+  systemctl enable --now clamav-daemon
+  freshclam || warn "ClamAV Signaturen konnten nicht aktualisiert werden"
 }
 
-start_services() {
-  log "Starte alle Dienste ..."
-  service pf start || warn "pf konnte nicht gestartet werden"
-  service isc-dhcpd start || warn "DHCP konnte nicht gestartet werden"
-  service sshd start || warn "sshd konnte nicht gestartet werden"
-  service samba_server start || warn "Samba konnte nicht gestartet werden"
-  service hostapd start || warn "hostapd konnte nicht gestartet werden"
-  service clamav-clamd start || warn "ClamAV konnte nicht gestartet werden"
+setup_nat_firewall() {
+  log "Aktiviere NAT über iptables ..."
+  iptables -t nat -A POSTROUTING -o eno1 -j MASQUERADE
+  iptables -A FORWARD -i eno1 -o enp7s0 -m state --state RELATED,ESTABLISHED -j ACCEPT
+  iptables -A FORWARD -i enp7s0 -o eno1 -j ACCEPT
+
+  iptables-save >/etc/iptables/iptables.rules
+  systemctl enable --now iptables
 }
 
 #=======================#
@@ -227,20 +173,12 @@ main() {
   install_pkg_programme
   install_npm_programme
   set_router_rechte
-  setup_sudo
-  setup_netwerk
+  setup_netzwerk
   create_dhcpd_conf
   create_hostapd_conf
-  create_smb4_conf
-  stop_services
-  setup_dhcp
-  setup_ssh
-  setup_wlan_ap
-  setup_samba
-  setup_firewall_pf
-  setup_clamav
-  stop_services
-  start_services
+  create_smb_conf
+  setup_nat_firewall
+  setup_dienste
 }
 
 main
