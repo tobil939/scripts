3da00715628ff8d2747a14d6508f0dbae69ca4ad
Tobi L <tobias.leitz@gmx.de>
2026-02-03 21:11:59 +0100

    latest check

--- router.sh
+++ router.sh
@@ -1,74 +1,53 @@
-```bash
 #!/usr/bin/env bash
-# Script: router2.sh
-# Author: tobil
-# Date: 2026-01-25_20:44:28
+# Script: Redone.sh
+# Author: tobil / grok
+# Date: 2026-02-02
 # License: MIT
-# Description: Installation script for setting up my router, reworked with grok
-# Version: v8.0
-# Variables
-user=${SUDO_USER:-$(whoami)}
-if [ "$user" == "root" ]; then user="tobirouter"; fi
-group=$(groups | awk '{print $1}')
+# Description: Installation script for setting up router on Arch Linux (not Rocky)
+# Version: v9.0
+#
+### Variables
+fullname=$PWD
+user=${PWD#/home/}
+group=$user
 drive1="sda1"
-drive2="nvme0n1p2"
-UUID1=$(sudo blkid -s UUID -o value /dev/"$drive1")
-UUID2=$(sudo blkid -s UUID -o value /dev/"$drive2")
+drive2="nvme0n1p2" # Fixed typo
+UUID1=$(lsblk -f | grep "$drive1" | awk '{print $3}')
+UUID2=$(lsblk -f | grep "$drive2" | awk '{print $3}')
 WAN_IF="eno1"
 LAN_ETH="enp7s0"
-LAN_WLAN="wlan0"
+LAN_WLAN="wlp5s0"
 LAN_IP="10.10.10.1/24"
 DHCP_RANGE="10.10.10.50,10.10.10.250,12h"
 SSID="bunga"
 WPA_PASS="bungahart"
 COUNTRY_CODE="DE"
-# Logfile
-logfile="/home/$user/logging/install.log"
-logpath="/home/$user/logging/"
-checkinstall() {
-  horst="0"
-  arch=false
-  rocky=false
-  command -v pacman >/dev/null && horst="1"
-  command -v dnf >/dev/null && horst="2"
-  if [[ "$horst" == 0 ]]; then
-    log "not checked for packages manager"
-  elif [[ "$horst" == 1 ]]; then
-    arch=true
-    log "OS is Arch"
-  elif [[ "$horst" == 2 ]]; then
-    rocky=true
-    log "OS is Rocky"
-  fi
-}
-# Get timestamp
+echo $fullname
+echo $user
+echo $group
+echo $UUID1
+echo $UUID2
+needed=(
+  "neovim"
+  "iw"
+  "less"
+  "htop"
+  "openssh"
+  "fzf"
+  "hostapd"
+  "dnsmasq"
+  "dhcpcd"
+  "samba"
+)
+logfile="$fullname/logging/router-setup.log"
+mkdir -p "$(dirname "$logfile")"
+### Get timestamp
 get_timestamp() {
   timestamp=$(date +"%Y-%m-%d_%H:%M:%S")
 }
-# Initialize logging
-logging() {
-  get_timestamp
-  echo "$timestamp Log path: $logpath"
-  echo "$timestamp Log file: $logfile"
-  # Check/create logfile
-  if [[ -f "$logfile" ]]; then
-    get_timestamp
-    echo "$timestamp Logfile exists" | tee -a "$logfile"
-  else
-    get_timestamp
-    echo "$timestamp Creating logfile"
-    mkdir -p "$logpath" || {
-      echo "$timestamp Failed to create logpath"
-      exit 1
-    }
-    touch "$logfile" || {
-      echo "$timestamp Failed to create logfile"
-      exit 2
-    }
-    echo "$timestamp Logfile created" | tee -a "$logfile"
-  fi
-}
+### Logging
 log() {
+  get_timestamp
   echo "$timestamp $*" | tee -a "$logfile"
 }
 # Error handling
@@ -78,7 +57,7 @@ handling() {
   case $error in
     0) log "Script completed successfully" ;;
     1)
-      log "Directory error"
+      log "Problems installing progs"
       exit 1
       ;;
     2)
@@ -86,7 +65,7 @@ handling() {
       exit 2
       ;;
     3)
-      log "Cannot deactivate services"
+      log "Problems with logging"
       exit 3
       ;;
     4)
@@ -101,6 +80,46 @@ handling() {
       log "Cannot set forwarding IP"
       exit 6
       ;;
+    7)
+      log "Cannot configure dhcpcd"
+      exit 7
+      ;;
+    8)
+      log "Cannot configure bridge"
+      exit 8
+      ;;
+    9)
+      log "Cannot set NAT/Forwarding"
+      exit 9
+      ;;
+    10)
+      log "Cannot start WAN DHCP"
+      exit 10
+      ;;
+    11)
+      log "Cannot configure dnsmasq"
+      exit 11
+      ;;
+    12)
+      log "Cannot bridge WLAN"
+      exit 12
+      ;;
+    13)
+      log "Cannot configure Samba"
+      exit 13
+      ;;
+    14)
+      log "Cannot setup Btrfs"
+      exit 14
+      ;;
+    15)
+      log "Cannot configure hostapd"
+      exit 15
+      ;;
+    16)
+      log "Cannot override systemd"
+      exit 16
+      ;;
     *)
       log "Unknown error: $error"
       exit "$error"
@@ -109,123 +128,67 @@ handling() {
 }
 deactivated() {
   log "Stopping services"
-  log "Stop NetworkManager"
-  sudo systemctl is-active --quiet NetworkManager && systemctl disable --now NetworkManager || true
-  log "Stop iptables"
-  sudo systemctl is-active --quiet iptables && systemctl stop iptables || true
-  log "Stop dhcpcd"
-  sudo systemctl is-active --quiet "dhcpcd@$WAN_IF" && systemctl stop "dhcpcd@$WAN_IF" || true
-  log "Stop dnsmasq"
-  sudo systemctl is-active --quiet dnsmasq && systemctl stop dnsmasq || true
-  log "Stop hostapd"
-  sudo systemctl is-active --quiet hostapd && systemctl stop hostapd || true
-  log "Stop smb"
-  sudo systemctl is-active --quiet smb && systemctl stop smb || true
-  log "Stop nmb"
-  sudo systemctl is-active --quiet nmb && systemctl stop nmb || true
-  log "Stop ssh"
-  sudo systemctl is-active --quiet sshd && systemctl stop sshd || true
+   systemctl disable --now NetworkManager || true
+   systemctl stop iptables || true
+   systemctl stop "dhcpcd@$WAN_IF" || true
+   systemctl stop dnsmasq || true
+   systemctl stop hostapd || true
+   systemctl stop smb || true
+   systemctl stop nmb || true
+   systemctl stop sshd || true
 }
 activated() {
-  log "starting networkcards"
-  log "br0 to bridge"
-  sudo ip link add br0 type bridge || true
-  log "enp7s0 to master br0"
-  sudo ip link set enp7s0 up
-  sudo ip link set enp7s0 master br0
-  log "wlan0 to master bro"
-  sudo ip link set wlan0 up
-  sudo iw dev wlan0 set 4addr on || true
-  sudo ip link set wlan0 master br0
-  log "starting bridge"
-  sudo ip link set br0 up
-  log "setting ip"
-  sudo ip addr flush dev br0 || true
-  sudo ip addr add 10.10.10.1/24 dev br0
-  log "starting services"
-  log "Disable NetworkManager"
-  sudo systemctl disable --now NetworkManager || true
-  log "Start iptables"
-  sudo systemctl start iptables || true
-  log "Start dhcpcd"
-  sudo systemctl start "dhcpcd@$WAN_IF" || true
-  log "Start dnsmasq"
-  sudo systemctl start dnsmasq || true
-  log "Start hostapd"
-  sudo systemctl start hostapd || true
-  log "Start smb"
-  sudo systemctl start smb || true
-  log "Start nmb"
-  sudo systemctl start nmb || true
-  log "Start ssh"
-  sudo systemctl start sshd || true
+  log "Starting network cards"
+   ip link add br0 type bridge || true
+   ip link set "$LAN_ETH" up
+   ip link set "$LAN_ETH" master br0
+   ip link set "$LAN_WLAN" up
+   iw dev "$LAN_WLAN" set 4addr on || true
+   ip link set "$LAN_WLAN" master br0
+   ip link set br0 up
+   ip addr flush dev br0 || true
+   ip addr add "$LAN_IP" dev br0
+  log "Starting services"
+   systemctl disable --now NetworkManager || true
+   systemctl start iptables || true
+   systemctl start "dhcpcd@$WAN_IF" || true
+   systemctl start dnsmasq || true
+   systemctl start hostapd || true
+   systemctl start smb || true
+   systemctl start nmb || true
+   systemctl start sshd || true
 }
+### Pakete
 installprog() {
-  log "Installing packages"
-  needed=(
-    "fzf"
-    "polkit"
-    "samba"
-    "python"
-    "python-pip"
-    "iw"
-    "hostapd"
-    "dnsmasq"
-    "dhcpcd"
-    "openssh"
-    "less"
-    "htop"
-    "vim"
-    "neovim"
-  )
-  if "$arch"; then
-    log "Installing yay"
-    sudo pacman -S --needed --noconfirm git base-devel
-    if [[ ! -d "yay" ]]; then
-      sudo -u "$user" git clone https://aur.archlinux.org/yay.git
-      cd yay || exit 2
-      sudo -u "$user" makepkg -si --noconfirm --needed || exit 2
-      cd "/home/$user" || exit 2
-    fi
-    log "Installing programs"
-    for prog in "${needed[@]}"; do
-      log "$prog will be installed"
-      yay -S "$prog" --needed --noconfirm || exit 3
-    done
-    log "Updating yay"
-    #yay -Syu --noconfirm || exit 3
-  elif "$rocky"; then
-    log "installing with dnf, activating epel-release"
-    sudo dnf install -yq epel-release
-    for prog in "${needed[@]}"; do
-      log "$prog will be installed"
-      sudo dnf install -yq "$prog" || exit 3
-    done
-    log "updating the system"
-    sudo dnf update -yq || exit 3
-  else
-    exit 194
+  log "Installing base-devel and git"
+   pacman -S --needed --noconfirm base-devel git || exit 1
+  if ! command -v yay &>/dev/null; then
+    sudo -u "$user" git clone https://aur.archlinux.org/yay.git || exit 1
+    cd yay || exit 1
+    sudo -u "$user" makepkg -si --noconfirm || exit 1
+    cd .. || exit 1
   fi
+  for prog in "${needed[@]}"; do
+    log "Installing $prog"
+    yay -S --needed --noconfirm "$prog" || exit 1
+  done
+  log "updating"
+  #yay -Syu --noconfirm || exit 1
 }
+### Cleanup
+cleanup() {
+  log "Cleaning up"
+  yay -Ycc --noconfirm || true
+}
+### Creating bashrc
 bashing() {
   log "Setting up bashrc"
-  cat >>"/home/$user/.bashrc" <<'HERE'
+  tee "$fullname/.bashrc" <<'HERE'
 # ~/.bashrc
-#
-# If not running interactively, don't do anything
 [[ $- != *i* ]] && return
 alias ls='ls --color=auto'
 alias grep='grep --color=auto'
 PS1='[\u@\h \W]\$ '
-# ~/.bashrc
-#
-# If not running interactively, don't do anything
-[[ $- != *i* ]] && return
-PS1='[\u@\h \W]\$ '
-#export GREP_COLORS='mt=1;35'
-#export LS_COLORS="di=1;35:fi=0:ln=36"
-alias grep='grep --color=auto'
-alias ls='ls --color=auto'
 alias sl='ls -lh --color=auto'
 fuzzy() {
   local file
@@ -243,10 +206,7 @@ trs() {
   done
 }
 bashing() {
-  local data
-  local path
-  local file
-  local oldpath
+  local data path file oldpath
   oldpath=$(pwd)
   data="$1"
   [[ -z "$data" ]] && exit 91
@@ -271,7 +231,7 @@ bashing() {
     chmod +x "$file" || exit 90
     ls -lh
   else
-    mkdir -p "$path"
+    mkdir -p "$path" || exit 88
     cd "$path" || exit 88
     touch "$file" || exit 89
     chmod +x "$file" || exit 90
@@ -287,30 +247,23 @@ bashing() {
 EOF
   cat <<'EOF' >>"$file"
 # Logfile
-logfile="$user/logging/logfile.log"
-logpath="$user/logging/"
-# Get timestamp
+logfile="$HOME/logging/logfile.log"
+logpath="$HOME/logging/"
 get_timestamp() {
   timestamp=$(date +"%Y-%m-%d_%H:%M:%S")
 }
-# Initialize logging
 logging() {
   get_timestamp
   echo "$timestamp Log path: $logpath" | tee -a "$logfile"
   echo "$timestamp Log file: $logfile" | tee -a "$logfile"
-  # Check/create logfile
   if [[ -f "$logfile" ]]; then
-    get_timestamp
     echo "$timestamp Logfile exists" | tee -a "$logfile"
   else
-    get_timestamp
-    echo "$timestamp Creating logfile" | tee -a "$logfile"
     mkdir -p "$logpath" || { echo "$timestamp Failed to create logpath" | tee -a "$logfile"; exit 1; }
     touch "$logfile" || { echo "$timestamp Failed to create logfile" | tee -a "$logfile"; exit 2; }
     echo "$timestamp Logfile created" | tee -a "$logfile"
   fi
 }
-# Error handling
 handling() {
   local error="$1"
   get_timestamp
@@ -328,64 +281,62 @@ EOF
 }
 HERE
 }
-sshing(){
+### SSH
+sshing() {
   log "Enabling SSH"
-  sudo systemctl enable --now sshd
-  }
+  systemctl enable --now sshd || exit 4
+}
+### WLAN freischalten
+wlan() {
+  log "WLAN freischalten"
+  rfkill unblock wifi 2>/dev/null || exit 4
+}
+### NetworkManager deaktivieren
+networkdeac() {
+  log "NetworkManager deaktivieren"
+  systemctl disable --now NetworkManager 2>/dev/null || exit 4
+}
+### IP Forwarding
 forwarding() {
-  ### IP Forwarding
-  log "Enabling IP Forwarding"
-  cat <<EOF >/etc/sysctl.d/99-ipforward.conf
-net.ipv4.ip_forward=1
-EOF
+  log "IP Forwarding aktivieren"
+  tee /etc/sysctl.d/99-ipforward.conf <<<"net.ipv4.ip_forward=1" || exit 6
   sysctl --system || exit 6
 }
-setdhcp() {
-  ### Limit dhcpcd to WAN
-  log "Configuring dhcpcd"
-  cat <<EOF >/etc/dhcpcd.conf
-denyinterfaces br0
-denyinterfaces $LAN_ETH
-denyinterfaces $LAN_WLAN
-EOF
+### dhcpcd auf WAN begrenzen
+dhcp() {
+  log "dhcpcd konfigurieren"
+  tee /etc/dhcpcd.conf <<<"denyinterfaces br0 $LAN_ETH $LAN_WLAN" || exit 7
 }
-bridging() {
-  ### Bridge
-  log "Configuring bridge"
-  ip link show br0 &>/dev/null || ip link add br0 type bridge
-  ip link set br0 up
-  ip link set "$LAN_ETH" up
-  ip link set "$LAN_ETH" master br0 || exit 6
-  ip addr flush dev br0 || exit 6
-  ip addr replace "$LAN_IP" dev br0
+### Bridge
+bridge() {
+  log "Bridge konfigurieren"
+  ip link show br0 &>/dev/null || ip link add br0 type bridge || exit 8
+   ip link set br0 up || exit 8
+   ip link set "$LAN_ETH" up || exit 8
+   ip link set "$LAN_ETH" master br0 2>/dev/null || exit 8
+   ip addr flush dev br0 2>/dev/null || exit 8
+   ip addr add "$LAN_IP" dev br0 || exit 8
 }
+### NAT + Forwarding
 nat() {
-  ### NAT + Forwarding (idempotent)
-  log "Configuring iptables NAT & Forwarding"
-  iptables -t nat -C POSTROUTING -o "$WAN_IF" -j MASQUERADE 2>/dev/null \
-    || iptables -t nat -A POSTROUTING -o "$WAN_IF" -j MASQUERADE
-  iptables -C FORWARD -i "$WAN_IF" -o br0 -m state --state RELATED,ESTABLISHED -j ACCEPT 2>/dev/null \
-    || iptables -A FORWARD -i "$WAN_IF" -o br0 -m state --state RELATED,ESTABLISHED -j ACCEPT
-  iptables -C FORWARD -i br0 -o "$WAN_IF" -j ACCEPT 2>/dev/null \
-    || iptables -A FORWARD -i br0 -o "$WAN_IF" -j ACCEPT
-  mkdir -p /etc/iptables
-  iptables -P INPUT DROP
-  iptables -A INPUT -i lo -j ACCEPT
-  iptables -A INPUT -i br0 -p tcp --dport 22 -j ACCEPT
-  iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
-  iptables-save >/etc/iptables/iptables.rules
-  systemctl enable --now iptables
+  log "iptables NAT & Forwarding"
+  iptables -t nat -C POSTROUTING -o "$WAN_IF" -j MASQUERADE 2>/dev/null || iptables -t nat -A POSTROUTING -o "$WAN_IF" -j MASQUERADE || exit 9
+  iptables -C FORWARD -i "$WAN_IF" -o br0 -m state --state RELATED,ESTABLISHED -j ACCEPT 2>/dev/null || iptables -A FORWARD -i "$WAN_IF" -o br0 -m state --state RELATED,ESTABLISHED -j ACCEPT || exit 9
+  iptables -C FORWARD -i br0 -o "$WAN_IF" -j ACCEPT 2>/dev/null || iptables -A FORWARD -i br0 -o "$WAN_IF" -j ACCEPT || exit 9
+   mkdir -p /etc/iptables || exit 9
+   iptables-save >/etc/iptables/iptables.rules || exit 9
+   systemctl enable --now iptables || exit 9
 }
-wan() {
-  ### WAN DHCP
-  log "Bringing up WAN + DHCP"
-  ip link set "$WAN_IF" up
-  systemctl enable --now "dhcpcd@$WAN_IF"
+### WAN DHCP
+wandhcp() {
+  log "WAN hochfahren + DHCP"
+  ip link set "$WAN_IF" up || exit 10
+  systemctl enable --now "dhcpcd@$WAN_IF" || exit 10
 }
+### dnsmasq
 dnsmasq() {
-  ### dnsmasq
-  log "Configuring dnsmasq"
-  cat <<EOF >/etc/dnsmasq.conf
+  log "dnsmasq konfigurieren"
+  tee /etc/dnsmasq.conf <<EOF || exit 11
 interface=br0
 bind-interfaces
 dhcp-range=$DHCP_RANGE
@@ -393,21 +344,19 @@ domain-needed
 bogus-priv
 log-dhcp
 EOF
-  systemctl enable --now dnsmasq
+  systemctl enable --now dnsmasq || exit 11
 }
-wlansetup() {
-  ### WLAN unblock (not aggressive!)
-  log "Unblocking WLAN"
-  rfkill unblock wifi || exit 4
-  ### WLAN + Bridge
-  log "Activating & bridging WLAN"
-  ip link set "$LAN_WLAN" up
-  iw dev "$LAN_WLAN" set 4addr on || log "4addr nicht moeglich"
-  ip link set "$LAN_WLAN" master br0 || exit 5
+### WLAN + Bridge
+wlanbridge() {
+  log "WLAN aktivieren & bridgen"
+  ip link set "$LAN_WLAN" up || exit 12
+  iw dev "$LAN_WLAN" set 4addr on || exit 12
+  ip link set "$LAN_WLAN" master br0 2>/dev/null || exit 12
 }
+### Samba-Shares
 samba() {
-  ### Samba-Shares (router1 + router2)
-  sudo cat <<EOF >/etc/samba/smb.conf
+  log "Samba konfigurieren"
+  tee /etc/samba/smb.conf <<EOF || exit 13
 [global]
 workgroup = WORKGROUP
 server string = Router Shares
@@ -435,24 +384,46 @@ force group = $group
 create mask = 0644
 directory mask = 0775
 EOF
-log "Setting up Btrfs subvolumes & mounts"
-  # Erst Verzeichnisse + Subvolumes anlegen
-  #sudo mkdir -p /router1 /router2
-  #sudo btrfs subvolume show /router1 2>/dev/null || sudo btrfs subvolume create /router1
-  #sudo btrfs subvolume show /router2 2>/dev/null || sudo btrfs subvolume create /router2
-  # fstab nur ergänzen, wenn nicht vorhanden
-  #echo "UUID=$UUID1 /router1 btrfs subvol=router1,defaults 0 2" | sudo tee -a /etc/fstab
-  #echo "UUID=$UUID2 /router2 btrfs subvol=router2,defaults 0 2" | sudo tee -a /etc/fstab
-  #sudo systemctl daemon-reload
-  #sudo mount -a || { log "mount -a fehlgeschlagen – siehe dmesg"; exit 1; }
-  #sudo chown -R "$user:$group" /router1 /router2
-  #sudo chmod -R 775 /router1 /router2
-  sudo systemctl enable --now smb nmb
 }
+### Btrfs-Subvolumes & Mounts
+btrfssetup() {
+  log "Btrfs-Subvolumes & Mounts einrichten"
+  log "checking for router1"
+  if btrfs subvolume show /router1 2>/dev/null; then
+  log "router1 found"
+  else
+   log "router1 not found"
+   log "mounting /dev/$drive1"
+   mount /dev/"$drive1" /mnt || exit 14
+   log "creating /mnt/router1"
+   btrfs subvolume create /mnt/router1 || exit 14
+   log "unmount /mnt"
+   umount /mnt
+  fi
+   if btrfs subvolume show /router2 2>/dev/null; then
+   log "router2 found"
+   else
+   log "router2 not found"
+   log "mounting /dev/$drive2"
+   mount /dev/"$drive2" /mnt || exit 14
+   log "creating /mnt/router2"
+   btrfs subvolume create /mnt/router2 || exit 14
+   log "unmount /mnt"
+   umount /mnt
+   fi
+   mkdir -p /router1 /router2 || exit 14
+   grep -q router1 /etc/fstab || echo "UUID=$UUID1 /router1 btrfs subvol=router1,defaults 0 2" | tee -a /etc/fstab || exit 14
+   grep -q router2 /etc/fstab || echo "UUID=$UUID2 /router2 btrfs subvol=router2,defaults 0 2" | tee -a /etc/fstab || exit 14
+   systemctl daemon-reload || exit 14
+   mount -a || exit 14
+   chown -R "$user:$group" /router1 /router2 || exit 14
+   chmod -R 775 /router1 /router2 || exit 14
+   systemctl enable --now smb nmb || exit 14
+}
+### hostapd
 hostapd() {
-  ### hostapd
-  log "Configuring hostapd"
-  cat <<EOF >/etc/hostapd/hostapd.conf
+  log "hostapd konfigurieren"
+  tee /etc/hostapd/hostapd.conf <<EOF || exit 15
 interface=$LAN_WLAN
 bridge=br0
 driver=nl80211
@@ -474,71 +445,40 @@ wpa_pairwise=CCMP
 rsn_pairwise=CCMP
 wpa_disable_eapol_key_retries=1
 EOF
-  echo 'DAEMON_CONF="/etc/hostapd/hostapd.conf"' >/etc/conf.d/hostapd
+  tee /etc/conf.d/hostapd <<<'DAEMON_CONF="/etc/hostapd/hostapd.conf"' || exit 15
 }
-setdrives() {
-  ### systemd-Override for WLAN-Device
-  log "Hostapd systemd override"
-  mkdir -p /etc/systemd/system/hostapd.service.d
-  cat <<EOF >/etc/systemd/system/hostapd.service.d/override.conf
+### systemd-Override
+systemdover() {
+  log "hostapd systemd-Override"
+  mkdir -p /etc/systemd/system/hostapd.service.d || exit 16
+  tee /etc/systemd/system/hostapd.service.d/override.conf <<EOF || exit 16
 [Unit]
 BindsTo=sys-subsystem-net-devices-$LAN_WLAN.device
 After=sys-subsystem-net-devices-$LAN_WLAN.device
 EOF
-  sudo chown -R "$user":"$group" /router1
-  sudo chown -R "$user":"$group" /router2
-  systemctl daemon-reload
-  systemctl enable --now hostapd
-}
-cleanup() {
-  log "Cleaning up"
-  if "$arch"; then
-    yay -Ycc
-  elif "$rocky"; then
-    sudo dnf clean all
-    sudo dnf autoremove
-  fi
-}
-autostart() {
-  log "Creating systemd Autostart Service"
-  cat <<EOF >/etc/systemd/system/router.service
-[Unit]
-Description=Router Auto Setup Script
-After=network-pre.target
-Wants=network-pre.target
-[Service]
-Type=oneshot
-RemainAfterExit=yes
-ExecStart=/root/router2.sh
-User=root
-StandardOutput=journal+console
-StandardError=journal+console
-[Install]
-WantedBy=multi-user.target
-EOF
-  log "Enabling router service"
-  systemctl daemon-reload
-  systemctl enable router.service
-  log "Systemd service created and enabled"
+  systemctl daemon-reload || exit 16
+  systemctl enable --now hostapd || exit 16
 }
 trap 'handling $?' EXIT
-logging
+log "=== Router-Setup Start ==="
 deactivated
-activated
-checkinstall
 installprog
 bashing
 sshing
+wlan
+networkdeac
 forwarding
-setdhcp
-bridging
+dhcp
+bridge
 nat
-wan
+wandhcp
 dnsmasq
-wlansetup
+wlanbridge
 samba
+btrfssetup
 hostapd
-setdrives
-autostart
+systemdover
+activated
 cleanup
-```
+log "=== Fertig. Reboot empfohlen. ==="
+
