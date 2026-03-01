d3860c95b603ce415038a853547475f13b292c17
Tobi L <tobias.leitz@gmx.de>
2026-03-01 22:47:45 +0100

    Update Brother printer and scanner installation script

--- testbrother.sh
+++ testbrother.sh
@@ -1,32 +1,30 @@
 #!/usr/bin/env bash
 set -e
 
-# Pakete installieren
+# Pakete
 sudo pkg update
 sudo pkg install -y cups cups-filters ghostscript10 avahi-app nss_mdns sane-backends sane-airscan ipp-usb simple-scan epiphany rpm4 bash linux_base-rl9
 
-# Linux-Emulation aktivieren
+# Linux-Emulation
 sudo sysrc linux_enable=YES
 sudo kldload linux linux64
 sudo service linux start
 
-# Dienste aktivieren/starten
-sudo sysrc cupsd_enable=YES avahi_daemon_enable=YES ippusb_enable=YES
+# Dienste
+sudo sysrc cupsd_enable=YES avahi_daemon_enable=YES ipp-usb_enable=YES
 sudo service cupsd start
 sudo service avahi-daemon start
-sudo service ippusb start
+sudo service ipp-usb start
 
-# Drucker-Treiber installieren
+# Drucker-Treiber
 cd /tmp
-fetch https://download.brother.com/welcome/dlf105958/mfcl2860dwlpr-4.1.0-1.i386.rpm
-fetch https://download.brother.com/welcome/dlf105959/mfcl2860dwcupswrapper-4.1.0-1.i386.rpm
-rpm2cpio mfcl2860dwlpr-4.1.0-1.i386.rpm | sudo cpio -idmv
-rpm2cpio mfcl2860dwcupswrapper-4.1.0-1.i386.rpm | sudo cpio -idmv
-sudo sed -i.bak 's/chown lp/chown root/' /opt/brother/Printers/MFCL2860DW/inf/setupPrintcap
-sudo sed -i.bak 's/chgrp lp/chgrp daemon/' /opt/brother/Printers/MFCL2860DW/inf/setupPrintcap
-sudo /opt/brother/Printers/MFCL2860DW/inf/setupPrintcap MFCL2860DW -i USB
+fetch https://download.brother.com/welcome/dlf106067/mfcl2860dwepdrv-4.1.0-1.i386.rpm
+rpm2cpio mfcl2860dwepdrv-4.1.0-1.i386.rpm | sudo cpio -idmv
+sudo sed -i.bak 's/chown lp/chown root/' /opt/brother/Printers/MFCL2860DWE/inf/setupPrintcap
+sudo sed -i.bak 's/chgrp lp/chgrp daemon/' /opt/brother/Printers/MFCL2860DWE/inf/setupPrintcap
+sudo /opt/brother/Printers/MFCL2860DWE/inf/setupPrintcap MFCL2860DWE -i USB
 
-# Scanner-Treiber installieren
+# Scanner-Treiber
 fetch https://download.brother.com/welcome/dlf104036/brscan5-1.5.1-0.x86_64.rpm
 rpm2cpio brscan5-1.5.1-0.x86_64.rpm | sudo cpio -idmv
 sudo ln -s /opt/brother/scanner/brscan5/libSane-brscan5.so.1.0.0 /usr/local/lib/sane/libsane-brscan5.so.1
@@ -42,7 +40,7 @@ sudo /opt/brother/scanner/brscan5/brsaneconfig5 -a name=MFC-L2860DWE model=MFC-L
 echo "brscan5" | sudo tee -a /usr/local/etc/sane.d/dll.conf
 echo "airscan" | sudo tee -a /usr/local/etc/sane.d/dll.conf
 
-# devd.conf für Permissions
+# devd.conf
 cat << EOF | sudo tee /usr/local/etc/devd/brother.conf
 notify 100 {
     match "system" "USB";
@@ -64,8 +62,8 @@ sudo pw groupmod saned -m "$USERNAME"
 sudo pkg upgrade -y
 sudo pkg autoremove -y
 
-# Tools öffnen
+# Tools
 epiphany http://localhost:631 &
 simple-scan &
 
-echo "Fertig. Teste: lp -d MFCL2860DW /usr/share/cups/testprint.ps; scanimage -L"
+echo "Fertig. Teste: lp -d MFCL2860DWE /usr/share/cups/testprint.ps; scanimage -L"
